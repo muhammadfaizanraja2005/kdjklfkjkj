623d9996116a7ff3806d6715e5ae76acdc62e95e
muhammadfaizanraja2005 <124033350+muhammadfaizanraja2005@users.noreply.github.com>
2023-12-11 14:39:30 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+//loops retype chla ga program multuple time chala ga
+//i++ == increment
+//window ka button +. emoji khol jayaga 
+// const std =["ali","ahmed","uzair"]
+
+// for (let i = 0; i < 5 ;i++) {
+//     console.log(`${std[i]} hi`)
+// }
+// for(let a = 1;a<=10;a++){
+//  console.log("10","X",a,"=",a*10)
+// }
+// //NESTED LOOP for loop
+// for (let i = 0; i < 3 ;i++) {
+//     console.log("parent loop",i)
+
+// for (let a = 0; a < 4 ;a++) {
+//     console.log("child loop",a)
+// }
+const videos=[
+    {
+        tittle:"video1",
+        
+    }
+]
